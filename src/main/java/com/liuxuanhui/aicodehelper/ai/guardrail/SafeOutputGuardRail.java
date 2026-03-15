22132a617a4297c7d08a516723ef4fb925dcf18d
Liu Xuanhui <1805173850@qq.com>
2026-03-15 17:06:51 +0800

    close and finish guardrail

---
+++ src/main/java/com/liuxuanhui/aicodehelper/ai/guardrail/SafeOutputGuardRail.java
@@ -0,0 +1,11 @@
+package com.liuxuanhui.aicodehelper.ai.guardrail;
+
+/**
+ * @Author:Liu Xuanhui
+ * @Description:
+ * @DateTime: 2026/3/15 16:28
+ * @Component:
+ */
+ 
+public class SafeOutputGuardRail {
+}
